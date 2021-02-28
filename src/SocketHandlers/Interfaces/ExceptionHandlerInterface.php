2d896e9b37cb7f21f453193dc8f84091b5291128
Savio Resende <savio@savioresende.com.br>
2021-02-28 11:54:43 -0700

    Updated the exception handler interface.

--- src/SocketHandlers/Interfaces/ExceptionHandlerInterface.php
+++ src/SocketHandlers/Interfaces/ExceptionHandlerInterface.php
@@ -9,8 +9,9 @@ interface ExceptionHandlerInterface
     /**
      * @param Exception $e          Current exception being handled.
      * @param array     $parsedData Parsed data at the current message.
+     * @param mixed     $fd         File descriptor (connection).
      * @param mixed     $server     The socket server instance useful for
      *                              sending back the expected error message.
      */
-    public function handle(Exception $e, array $data, $server = null);
+    public function handle(Exception $e, array $data, $fd = null, $server = null);
 }
